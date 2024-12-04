0cd63919249d54a890a331487f0da3613995f361
andrew-quintana <a.quintana94@gmail.com>
2024-12-04 13:07:22 -0800

    apply .gitignore rules

--- build/jetbot_pro/CTestTestfile.cmake
+++
@@ -1,6 +0,0 @@
-# CMake generated Testfile for 
-# Source directory: /home/jetbot/catkin_ws/src/jetbot_pro
-# Build directory: /home/jetbot/catkin_ws/build/jetbot_pro
-# 
-# This file includes the relevant testing commands required for 
-# testing this directory and lists subdirectories to be tested as well.
